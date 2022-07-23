6883a810692ed90aba3b3cc2a4007699cdc666a9
JackWorld <jackwong0326@gmail.com>
2022-07-23 21:35:01 +0800

    Add server and route.

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express =require('express');
+const cors =require('cors');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+const app = express();
+const con =require('./db/connectDB');
+
+app.use(cors());
+app.use(express.json());
+app.use(require('./routes/route'));
+
+con.then(db=>{
+    if(!db) return process.exit(1);
+
+    app.listen(port, ()=>{
+        console.log(`Server listening at port http://localhost:${port}`);
+    });
+
+    app.on('error', err => console.log(`Failed To Connect with HTTP Server: ${err}`))
+}).catch(error=>{console.log(`Connection Error: ${error}`)})
