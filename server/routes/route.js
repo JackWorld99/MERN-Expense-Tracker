9ef2db3e9b52ba80af4fc921b5c9258b13df97a6
JackWorld <jackwong0326@gmail.com>
2022-07-24 21:16:48 +0800

    Update route.

--- server/routes/route.js
+++ server/routes/route.js
@@ -1,6 +1,8 @@
 const routes = require("express").Router();
 const controller = require('../controllers/controller');
 
-routes.route('/api/categories').get(controller.getCategories);
+routes.route('/categories').post(controller.createCategories).get(controller.getCategories);
+routes.route('/transactions').post(controller.createTranscations).get(controller.getTranscations).delete(controller.deleteTranscations);
+routes.route('/lebels').get(controller.getLabels);
 
 module.exports = routes;
